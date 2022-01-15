3d662bc991eac56bab13559fc0d231135136015c
conagreen <icoqwee@gmail.com>
2022-01-16 01:07:29 +0900

    조회 빈이 2개 이상 - 문제

--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -13,7 +13,7 @@ public class AutoAppConfigTest {
 
     @Test
     void basicScan() {
-        final AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+z        final AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
