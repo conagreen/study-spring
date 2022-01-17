9fa8dd577b6e5210411889fa21f9340ebcd41073
conagreen <icoqwee@gmail.com>
2022-01-18 00:57:41 +0900

    스코프와 Provier - 웹 스코프 빈 생성문제 해결

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,17 +13,19 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
 
-    private final LogDemoServide logDemoServide;
-    private final MyLogger myLogger;
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+     public String logDemo(HttpServletRequest request) throws InterruptedException {
         final String requestURL = request.getRequestURL().toString();
+        final MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
-        logDemoServide.logic("testId");
+        Thread.sleep(1000);
+        logDemoService.logic("testId");
 
         return  "OK";
     }
